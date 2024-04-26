686fcf8c80f4749d7a8e18780e33a078ccf83661
voltarou <bagusadnan04@gmail.com>
2024-04-26 10:46:51 +0800

    if

--- okee/okee.cpp
+++ okee/okee.cpp
@@ -19,3 +19,14 @@ void addNode() {
 	cin >> nama;
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
