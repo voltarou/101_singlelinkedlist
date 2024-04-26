64002bc25c26be96a982ff706983ce33c1c4dfe8
voltarou <bagusadnan04@gmail.com>
2024-04-26 10:52:26 +0800

    while

--- okee/okee.cpp
+++ okee/okee.cpp
@@ -55,3 +55,16 @@ bool serachNode(int nim, Node* current, Node* previous) {
 		previous = current;
 		current = current->next;
 	}
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
